229d2f580f7e72c1889b555e296decd21b6af075
CliftonFestus <79002129+CliftonFestus@users.noreply.github.com>
2021-04-15 17:21:56 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -43,6 +43,11 @@ df = pd.DataFrame(distrubutions,
                  index = file_names)
 df[df['Num Vague Terms']!=0].to_csv('./Distrubution.csv')
 df[df['Num Vague Terms']==0].to_csv('./No_Terms.csv')
+Df["Name"] = Df.index
+Df["Name"] = Df["Name"].map(lambda x: str(x)[:-4])
+Df["Name"]= Df["Name"].str.replace("-"," ")
+Df["Name"]= Df["Name"].str.replace(","," ")
+Df.set_index('Name')
 
 
 # Plottinf the dataframe df
